fdf70cd49c23577ec6ed36c34dde5715a9d662e0
beingyoyo <sanjaymakwana02@gmail.com>
2019-04-14 15:33:02 +0530

    Calculating aspect ratio

--- Module-1.py
+++ Module-1.py
@@ -3,14 +3,14 @@ import pandas
 from PIL import Image
 
 filename = "C:/Users/Yash Makwana/Desktop/Project/13_left.jpeg"
-basewidth = 512
-baseheight = 512
 try:
 	with Image.open(filename) as image:
+		
+		
+		aspect_ratio = image.size[0] / image.size[1]
 		print("Width:", image.size[0])
 		print("Height:", image.size[1])
-
-		image.resize((basewidth, baseheight), PIL.Image.ANTIALIAS)
-		image.save('resized_13_left.jpeg')
+		#image.resize((basewidth, baseheight), PIL.Image.ANTIALIAS)
+		#image.save('resized_13_left.jpeg')
 except IOError as e:
 	raise e
