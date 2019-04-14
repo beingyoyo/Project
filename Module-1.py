0f761c84d21da0cb2ea457bf7de09bc5811a81c9
beingyoyo <sanjaymakwana02@gmail.com>
2019-04-14 18:43:30 +0530

    Final Re-size

--- Module-1.py
+++ Module-1.py
@@ -6,16 +6,14 @@ filename = "C:/Users/Yash Makwana/Desktop/Project/13_left.jpeg"
 new_width = 512
 try:
 	with Image.open(filename) as image:
-		
-		
 		aspect_ratio = image.size[0] / image.size[1]
 		print("Aspect Ratio:", aspect_ratio)
 		print("Width:", image.size[0])
 		print("Height:", image.size[1])
-
 		new_height = int((new_width / aspect_ratio))
-
-		cover = resizeimage.resize_cover(image, [512, 512], validate = False)
-		cover.save('13_left.jpeg', image.format)
+		print("New Width:", new_width)
+		print("New Height:", new_height)
+		image = resizeimage.resize_cover(image, [new_width, new_height], validate = False)
+		image.save('13_left.jpeg', image.format)
 except IOError as e:
 	raise e
