d12f3cd27d79d5bea311c58d8fbc2269a091d9b3
bhaven123 <naikbhaven11@gmail.com>
2019-05-06 20:21:09 +0530

    Added List of all Images

--- Module-1.py
+++ Module-1.py
@@ -1,4 +1,3 @@
-import PIL
 from resizeimage import resizeimage
 from PIL import Image
 ##############################################################
@@ -11,23 +10,28 @@ from PIL import Image
 #   new defined width of the image                           #
 ##############################################################
 
+list1 = ['10_left','10_right','13_left','13_right','15_left','15_right','16_left','16_right','17_left' ,'17_right']
+i = 0
+while i < len(list1):
+    list2 = list1[i]+'.jpeg'
+    i += 1
+    print(list2)
+    try:
+        filename = 'E:\\Project\\Diabetic Retinopathy\\check\\' + list2
+        new_width = 512
+        with Image.open(filename) as image:
 
-try:
-	filename = "C:/Users/Yash Makwana/Desktop/Project/13_left.jpeg"
-	new_width = 512
-	with Image.open(filename) as image:
+            aspect_ratio = image.size[0] / image.size[1]
+            print("Aspect Ratio:", aspect_ratio)
+            print("Width:", image.size[0])
+            print("Height:", image.size[1])
 
-		aspect_ratio = image.size[0] / image.size[1]
-		print("Aspect Ratio:", aspect_ratio)
-		print("Width:", image.size[0])
-		print("Height:", image.size[1])
+            new_height = int((new_width / aspect_ratio))
+            print("New Width:", new_width)
+            print("New Height:", new_height)
 
-		new_height = int((new_width / aspect_ratio))
-		print("New Width:", new_width)
-		print("New Height:", new_height)
+            image = resizeimage.resize_cover(image, [new_width, new_height], validate=False)
+            image.save(list2, image.format)
 
-		image = resizeimage.resize_cover(image, [new_width, new_height], validate = False)
-		image.save('13_left.jpeg', image.format)
-
-except IOError as e:
-	raise e
+    except IOError as e:
+        raise e
--- rgb.py
+++ rgb.py
@@ -1,17 +1,21 @@
-import PIL
 from PIL import Image
+list1 = ['10_left','10_right','13_left','13_right','15_left','15_right','16_left','16_right','17_left' ,'17_right']
+i = 0
+while i < len(list1):
+    list2 = list1[i]+'.jpeg'
+    i += 1
+    print(list2)
+    try:
+        filename = 'E:\\Project\\Diabetic Retinopathy\\check\\' + list2
 
-try:
-	filename = "C:/Users/sniha/Desktop/train/16_right.jpeg"
-	
-	with Image.open(filename) as image:
+        with Image.open(filename) as image:
 
-		#image.split() returns a tuple comprising of 3 images, one for each r, g & b
-		print(image.split())
+            #image.split() returns a tuple comprising of 3 images, one for each r, g & b
+            print(image.split())
 
-		image.split()[0].show()
-		image.split()[1].show()
-		image.split()[2].show()
+            image.split()[0].show()
+            image.split()[1].show()
+            image.split()[2].show()
 
-except IOError as e:
-	raise e
+    except IOError as e:
+        raise e
