d7ae1f0c2849656358eb26102d3a280dc3bda971
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2020-01-25 13:26:15 +0530

    Made changes to the image field

--- diabetic_retinopathy/dr_ui/models.py
+++ diabetic_retinopathy/dr_ui/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Image_upload(models.Model):
-    dr_image = models.ImageField(upload_to="images/")
+    dr_image = models.ImageField(upload_to="images/", blank=True)
