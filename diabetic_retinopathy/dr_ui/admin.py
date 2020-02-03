57583bd3b9a7af2d257bd9a01614820db7d6bdf1
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2020-02-03 21:29:12 +0530

    Changed the model name after flushing out

--- diabetic_retinopathy/dr_ui/admin.py
+++ diabetic_retinopathy/dr_ui/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Image_upload
+from .models import ImageUpload
 # Register your models here.
-admin.site.register(Image_upload)
+admin.site.register(ImageUpload)
